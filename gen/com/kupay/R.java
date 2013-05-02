7ddd0ad5ac88fe4efc12741d569e21bfac7789fb
Rodrigo Garcia <roy.gase@gmail.com>
2013-05-02 01:29:55 -0500

    añadi R y manifest para evitar errores de comipalcion

--- gen/com/kupay/R.java
+++ gen/com/kupay/R.java
@@ -10,6 +10,22 @@ package com.kupay;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int contents_text=0x7f040001;
+        public static final int encode_view=0x7f040002;
+        public static final int possible_result_points=0x7f040003;
+        public static final int result_image_border=0x7f040004;
+        public static final int result_minor_text=0x7f040005;
+        public static final int result_points=0x7f040006;
+        public static final int result_text=0x7f040007;
+        public static final int result_view=0x7f040008;
+        public static final int status_text=0x7f04000a;
+        public static final int status_view=0x7f040009;
+        public static final int transparent=0x7f040000;
+        public static final int viewfinder_frame=0x7f04000b;
+        public static final int viewfinder_laser=0x7f04000c;
+        public static final int viewfinder_mask=0x7f04000d;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,26 +33,97 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int blanco=0x7f020000;
+        public static final int ccbg=0x7f020001;
+        public static final int comp=0x7f020002;
+        public static final int comp2=0x7f020003;
+        public static final int home=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int ku=0x7f020006;
+        public static final int kucolor=0x7f020007;
+        public static final int opciones=0x7f020008;
+        public static final int tran=0x7f020009;
+        public static final int tran2=0x7f02000a;
+        public static final int vent=0x7f02000b;
+        public static final int vent2=0x7f02000c;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int qrImg=0x7f080000;
+        public static final int action_settings=0x7f060012;
+        /**  Messages IDs 
+         */
+        public static final int auto_focus=0x7f060000;
+        public static final int cantidad=0x7f060009;
+        public static final int decode=0x7f060001;
+        public static final int decode_failed=0x7f060002;
+        public static final int decode_succeeded=0x7f060003;
+        public static final int editando=0x7f060010;
+        public static final int headerq=0x7f060007;
+        public static final int imageView1=0x7f060008;
+        public static final int imageView2=0x7f06000a;
+        public static final int preview_view=0x7f06000c;
+        public static final int quit=0x7f060004;
+        public static final int realtabcontent=0x7f06000b;
+        public static final int restart_preview=0x7f060005;
+        public static final int return_scan_result=0x7f060006;
+        public static final int status_view=0x7f06000e;
+        public static final int tabsLayout=0x7f06000f;
+        public static final int text=0x7f060011;
+        public static final int viewfinder_view=0x7f06000d;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int bgcomp=0x7f030001;
+        public static final int capture=0x7f030002;
+        public static final int comp=0x7f030003;
+        public static final int decoder=0x7f030004;
+        public static final int tabs_bg=0x7f030005;
+        public static final int test=0x7f030006;
+        public static final int textcomp=0x7f030007;
+        public static final int tran=0x7f030008;
+        public static final int vent=0x7f030009;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f090000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f070001;
+        public static final int app_name=0x7f070000;
+        public static final int barcode_image=0x7f070021;
+        public static final int capture_name=0x7f070003;
+        public static final int contents_contact=0x7f070006;
+        public static final int contents_email=0x7f070007;
+        public static final int contents_location=0x7f070008;
+        public static final int contents_phone=0x7f070009;
+        public static final int contents_sms=0x7f07000a;
+        public static final int contents_text=0x7f07000b;
+        public static final int decoder_name=0x7f070005;
+        public static final int encoder_name=0x7f070004;
+        public static final int hello_world=0x7f070002;
+        public static final int msg_default_contents=0x7f07000c;
+        public static final int msg_default_format=0x7f07000d;
+        public static final int msg_default_meta=0x7f07000e;
+        public static final int msg_default_mms_subject=0x7f07000f;
+        public static final int msg_default_status=0x7f070010;
+        public static final int msg_default_time=0x7f070011;
+        public static final int msg_default_type=0x7f070012;
+        public static final int result_address_book=0x7f070013;
+        public static final int result_calendar=0x7f070014;
+        public static final int result_email_address=0x7f070015;
+        public static final int result_geo=0x7f070016;
+        public static final int result_isbn=0x7f070017;
+        public static final int result_product=0x7f070018;
+        public static final int result_sms=0x7f070019;
+        public static final int result_tel=0x7f07001a;
+        public static final int result_text=0x7f07001b;
+        public static final int result_uri=0x7f07001c;
+        public static final int result_wifi=0x7f07001d;
+        public static final int wifi_changing_network=0x7f07001e;
+        public static final int wifi_ssid_label=0x7f07001f;
+        public static final int wifi_type_label=0x7f070020;
     }
     public static final class style {
         /** 
@@ -49,10 +136,10 @@ public final class R {
             backward-compatibility can go here.
         
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f080001;
     }
 }
