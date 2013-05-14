0b280b354ddee73e6fc6cecda413157a7f5e0575
Rodrigo Garcia <roy.gase@gmail.com>
2013-05-13 20:50:38 -0500

    Carga las listas con XML, png de camara V1 instalado

---
+++ src/com/kupay/WeatherAdapter.java
@@ -0,0 +1,59 @@
+package com.kupay;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class WeatherAdapter extends ArrayAdapter<ventaw>{
+
+    Context context; 
+    int layoutResourceId;    
+    ventaw[] data = null;
+    
+    public WeatherAdapter(Context context, int layoutResourceId, ventaw[] data) {
+        super(context, layoutResourceId, data);
+        this.layoutResourceId = layoutResourceId;
+        this.context = context;
+        this.data = data;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View row = convertView;
+        WeatherHolder holder = null;
+        
+        if(row == null)
+        {
+            LayoutInflater inflater = ((Activity)context).getLayoutInflater();
+            row = inflater.inflate(layoutResourceId, parent, false);
+            
+            holder = new WeatherHolder();
+            holder.imgIcon = (ImageView)row.findViewById(R.id.imgIcon);
+            holder.txtTitle = (TextView)row.findViewById(R.id.txtTitle);
+            
+            row.setTag(holder);
+        }
+        else
+        {
+            holder = (WeatherHolder)row.getTag();
+        }
+        
+        ventaw ventaw = data[position];
+        holder.txtTitle.setText(ventaw.title);
+        holder.imgIcon.setImageResource(ventaw.icon);
+        
+        return row;
+    }
+    
+    static class WeatherHolder
+    {
+        ImageView imgIcon;
+        TextView txtTitle;
+    }
+}
---
+++ src/com/kupay/ventaw.java
@@ -0,0 +1,22 @@
+package com.kupay;
+
+import android.util.Log;
+
+
+	public class ventaw {
+		 
+		   public int icon;
+		    public String title;
+		    public ventaw(){
+		        super();
+		    }
+		    
+		    public ventaw (int icon, String title) {
+		    	
+		        super();
+		        this.icon = icon;
+		        this.title = title;
+		        Log.v("list","1w");
+		    }
+		}
+
