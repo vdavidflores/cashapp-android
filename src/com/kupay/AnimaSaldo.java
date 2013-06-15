27f055d4b827275bdf5338a1ba2130ce0a5d0a7d
VDavidFlores <victordavidfe@gmail.com>
2013-06-15 04:27:54 -0500

    añado algoritmos para hacer dinamica la animacion de cambio de saldo.

--- src/com/kupay/AnimaSaldo.java
+++ src/com/kupay/AnimaSaldo.java
@@ -15,6 +15,11 @@ class AnimaSaldo extends AsyncTask<Integer, Integer, Void>{
 	TextView cc;
 	int lastSaldo;
 	int nuevoSaldo;
+	long MAX_T = 60;
+	long MIN_T = 20;
+	long DIF_ESTANDAR = 200;
+	
+	
 	AnimaSaldo(Activity act){
 		this.actividad = act;
 	}
@@ -30,16 +35,29 @@ class AnimaSaldo extends AsyncTask<Integer, Integer, Void>{
 		 }catch (Exception e) {
 			lastSaldo = 0;
 		}
+		 
+		 
+		 
+	
 	  
       }
      
       protected Void doInBackground(Integer... params) {
     	  nuevoSaldo = params[0];
     	  int i= lastSaldo;
+    	 double DIF = Math.sqrt(Math.pow(lastSaldo-nuevoSaldo, 2));
+    	  if (DIF > DIF_ESTANDAR){
+    		  MAX_T = (long) ((DIF_ESTANDAR / DIF)*MAX_T); 
+    		  MIN_T = (long) ((DIF_ESTANDAR/DIF)*MAX_T);
+    		  
+    	  }
+    	  
     	 if (nuevoSaldo >= lastSaldo){
     		 while (i < nuevoSaldo) {
     			 try {
- 					Thread.sleep(30);
+    				
+    				 
+ 					Thread.sleep(map(i,lastSaldo,nuevoSaldo,MAX_T,MIN_T));
  				} catch (InterruptedException e) {
  					// TODO Auto-generated catch block
  					e.printStackTrace();
@@ -50,7 +68,7 @@ class AnimaSaldo extends AsyncTask<Integer, Integer, Void>{
     	 }else{
     		 while (i > nuevoSaldo) {
     			 try {
-					Thread.sleep(30);
+					Thread.sleep(map(i, nuevoSaldo, lastSaldo, MIN_T,MAX_T ));
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -63,6 +81,15 @@ class AnimaSaldo extends AsyncTask<Integer, Integer, Void>{
     	  
 		return null;
       }
+      
+      long map(long x, long in_min, long in_max, long out_min, long out_max)
+      {
+        long n = (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+        Log.v("app", "n: "+n+" in_min: "+in_min+" in_max: "+in_max+" out_min: "+out_min+" out_max: "+out_max);
+        return n;
+        
+      }
+      
     @Override
       protected void onProgressUpdate (Integer... valores) {
     	cc.setText("$"+valores[0].toString());
