c9c4bbcec59dcf7fdd197c0021ab6df1c944f555
Rodrigo <rodrigo@rodrigo-desktop.(none)>
2014-01-06 11:09:59 -0600

    rgreso de vacaciones

--- src/com/kupay/Llaveku.java
+++ src/com/kupay/Llaveku.java
@@ -39,20 +39,6 @@ public class Llaveku extends Fragment implements OnItemSelectedListener {
 
 		  
 		  
-		/* ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getActivity(),
-			        R.array.formas_de_depsito, android.R.layout.simple_spinner_item);
-			// Specify the layout to use when the list of choices appears
-			adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-			// Apply the adapter to the spinner
-			spinner.setAdapter(adapter);
-			
-			spinner.setOnItemSelectedListener(this );
-		 String [] values = 
-		        {"Time at Residence","Under 6 months","6-12 months","1-2 years","2-4 years","4-8 years","8-15 years","Over 15 years",};
-		    Spinner spinner = (Spinner) view.findViewById(R.id.spinner1);
-		    ArrayAdapter<String> LTRadapter = new ArrayAdapter<String>(getActivity().getApplicationContext(), android.R.layout.simple_spinner_item, values);
-		    LTRadapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
-		    spinner.setAdapter(LTRadapter);*/
 		 
 		 return view; 
 		 
@@ -82,19 +68,3 @@ public class Llaveku extends Fragment implements OnItemSelectedListener {
 	
 	
 }
-/*
-
-navicon.setOnClickListener(new OnClickListener() {
-	          public void onClick(View view) { 
-	        	  Activity act = getActivity();
-
-	        	  if(act instanceof MainConteiner) {
-	        	      ((MainConteiner) act).togle();
-	        	  }
-	         	 
-	          }} );
-
-
-
-
-*/
