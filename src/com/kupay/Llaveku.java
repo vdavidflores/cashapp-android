0cf571681b262598d9bdc836653f0e1dfb1a63a0
VDavidFlores <victordavidfe@gmail.com>
2014-01-29 02:21:04 -0600

    sistema de llave ku en movil

--- src/com/kupay/Llaveku.java
+++ src/com/kupay/Llaveku.java
@@ -1,8 +1,20 @@
 package com.kupay;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.kupay.Post.OnResponseAsync;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.InputType;
+import android.text.method.PasswordTransformationMethod;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,19 +23,24 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-public class Llaveku extends Fragment implements OnItemSelectedListener {
-	Button navicon;
-	
+public class Llaveku extends Fragment {
+	Button navicon, getllav;
+	EditText llavetext;
+	Post getServllav;
+	 int pin;
 	 @Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
 		 View view = View.inflate(getActivity().getApplicationContext(), R.layout.llaveku,null);
 		 
-		 Spinner spinner = (Spinner) view.findViewById(R.id.spinner1);
 		 
-		
+		 getServllav = new Post();
+		getllav = (Button) view.findViewById(R.id.getllave);
+		llavetext = (EditText) view.findViewById(R.id.llaveq);
 		  navicon = (Button)  view.findViewById(R.id.navicon_dep);
 		
 		  navicon.setOnClickListener(new OnClickListener() {
@@ -35,30 +52,121 @@ public class Llaveku extends Fragment implements OnItemSelectedListener {
 	        	  }
 	         	 
 	          }} );
-
+		  getllav.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+			 PIN();
+				
+			}
+		});
 
 		  
-		  
+		  getServllav.setOnResponseAsync(new OnResponseAsync() {
+			
+			@Override
+			public void onResponseAsync(JSONObject response) {
+				try {
+					if(response.getString("RESULTADO").equals("EXITO")){
+						Toast.makeText(getActivity(), "Nueva llave!", Toast.LENGTH_LONG).show();
+						
+						JSONObject datos = response.getJSONObject("DATOS");
+						llavetext.setText(datos.getString("llave_ku").toString());
+						
+					}else{
+						Toast.makeText(getActivity(), "Solicitud fallida!", Toast.LENGTH_LONG).show();
+					}
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+		});
 		 
 		 return view; 
 		 
 	 }
-	@Override
-	public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
-			long arg3) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void onNothingSelected(AdapterView<?> arg0) {
-		// TODO Auto-generated method stub
-		
-	}
 
+	 private void PIN(){
+			
+			AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+			builder.setIcon(R.drawable.pin);
+			builder.setTitle("Ingresa tu Pin");
+			builder.setMessage("Inserta tu Pin");
+			
+			
+			final EditText Password = new EditText(getActivity());
+			
+			builder.setView(Password);
+			Password.setGravity(Gravity.CENTER);
+		     Password.setHint("pin");
+		     Password.setWidth(200);
+			 Password.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+			 Password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+			 builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+				
+	            public void onClick(DialogInterface dialog, int id) {
+	         
+	                 pin = Integer.parseInt(Password.getEditableText().toString()) ;
+	                 obtenerLLAVE();
+	            }
+	        });
+			builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+	            public void onClick(DialogInterface dialog, int id) {
+	            	
+	            }
+	        });
+
+			AlertDialog dialog = builder.create();
+	    	dialog.show();
+	    
+	        	
+	    	
+	    }	
 	 
-	 
+	 private void obtenerLLAVE() {
+		JSONObject data = new JSONObject();
+		
+		
+		try {
+			data.put("usr", MiUsuario());
+			data.put("imei", MiImei());
+			data.put("pin", pin);
+			getServllav.setData(13,data);
+			getServllav.execAsync(getActivity().getApplicationContext());
+			
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
-
+	 private String MiUsuario(){
+	    	String usr = null;
+	        BDD dbh = new BDD(getActivity().getApplicationContext(),"kupay",null,1);
+	        SQLiteDatabase db= dbh.getReadableDatabase();
+	        Cursor reg = db.query("kupay",new String[]{"usr"},null,null,null,null,null,"1");
+	        if(reg.moveToFirst()){
+	            usr=reg.getString(0);
+	           
+	        
+	        }
+		 return usr;
+	    }
+	    
+	    private String MiImei(){
+	    	String imei = null;
+	        BDD dbh = new BDD(getActivity().getApplicationContext(),"kupay",null,1);
+	        SQLiteDatabase db= dbh.getReadableDatabase();
+	        Cursor reg = db.query("kupay",new String[]{"imei"},null,null,null,null,null,"1");
+	        if(reg.moveToFirst()){
+	            imei=reg.getString(0);
+	           
+	        
+	        }
+		 return imei;
+	    }
 	
 	
 	
