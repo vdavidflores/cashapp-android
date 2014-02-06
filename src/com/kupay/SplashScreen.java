8bf38b8aa1dafd62c25557eafaf78f7ddc68c7b3
VDavidFlores <victordavidfe@gmail.com>
2014-02-05 23:41:11 -0600

    reduccion de espera en splash screan

--- src/com/kupay/SplashScreen.java
+++ src/com/kupay/SplashScreen.java
@@ -10,7 +10,7 @@ import android.os.Handler;
 public class SplashScreen extends Activity {
  
     // Splash screen timer
-    private static int SPLASH_TIME_OUT = 5000;
+    private static int SPLASH_TIME_OUT = 2500;
  
     @Override
     protected void onCreate(Bundle savedInstanceState) {
